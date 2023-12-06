8072be300affb4111ff5d33df84aceeeca7b0d45
Wouter Stultiens <wouterstultiens@gmail.com>
2023-12-06 22:12:26 +0100

    added config file, created html pages for search and search_results, working Flask app

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import render_template, request
+from config import app, db
+from models import Recipe
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        ingredient = request.form['ingredient']
+        # Query the database for recipes containing the ingredient
+        recipes = Recipe.query.filter(Recipe.ingredients.like(f'%{ingredient}%')).all()
+        return render_template('search_results.html', recipes=recipes, ingredient=ingredient)
+    return render_template('search.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
