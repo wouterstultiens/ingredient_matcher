2502d2ab8a6e008af1fdda78ee37ee244c256a94
Wouter Stultiens <wouterstultiens@gmail.com>
2023-12-08 14:22:00 +0100

    small rework and added multiple tables to database

--- add_recipes.py
+++ add_recipes.py
@@ -1,17 +1,45 @@
+import random
 from app import app
-from models import Recipe
+from models import Recipe, Ingredient, RecipeStep
 from config import db
-import pandas as pd
 
-def add_sample_recipes():
-    d = {"id": [1, 2, 3, 4], "name": ["bami", "bami", "nasi", "nasi"], "ingredient": ["mie", "ei", "rijst", "ei"]}
-    df = pd.DataFrame(d)
+# Pools of sample data
+recipe_names = ["Spaghetti Bolognese", "Chicken Curry", "Vegetable Stir Fry", "Beef Stew", "Tomato Soup", "Caesar Salad", "Grilled Cheese Sandwich", "Pepperoni Pizza", "Pancakes", "Fish Tacos"]
+ingredients_pool = [
+    {"name": "spaghetti", "amount": "200g"},
+    {"name": "chicken", "amount": "200g"},
+    {"name": "carrots", "amount": "100g"},
+    {"name": "tomatoes", "amount": "150g"},
+    {"name": "lettuce", "amount": "50g"},
+    {"name": "cheese", "amount": "100g"},
+    {"name": "pepperoni", "amount": "70g"},
+    {"name": "flour", "amount": "200g"},
+    {"name": "fish", "amount": "150g"},
+    {"name": "tortilla", "amount": "2 pieces"}
+]
+steps_pool = [
+    "Boil water",
+    "Fry on pan",
+    "Bake in oven",
+    "Mix ingredients",
+    "Chop vegetables",
+    "Grill on barbecue",
+    "Marinate overnight",
+    "Simmer on low heat",
+    "Whisk together",
+    "Serve with sauce"
+]
 
-    for index, row in df.iterrows():
-        recipe = Recipe(id=row["id"], name=row["name"], ingredient=row["ingredient"])
+def add_sample_recipes(n):
+    for _ in range(n):
+        name = random.choice(recipe_names)
+        ingredients = [Ingredient(name=ingredient["name"], amount=ingredient["amount"]) for ingredient in random.sample(ingredients_pool, 3)]
+        steps = [RecipeStep(step=step) for step in random.sample(steps_pool, 3)]
+
+        recipe = Recipe(name=name, type="dinner", ingredients=ingredients, steps=steps)
         db.session.add(recipe)
 
     db.session.commit()
 
 with app.app_context():
-    add_sample_recipes()
+    add_sample_recipes(10)  # Specify number of recipes to add
--- models.py
+++ models.py
@@ -3,7 +3,17 @@ from config import db
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    ingredient = db.Column(db.Text, nullable=False)
+    type = db.Column(db.String(50))
+    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
+    steps = db.relationship('RecipeStep', backref='recipe', lazy=True)
 
-    def __repr__(self):
-        return f'<Recipe {self.name}>'
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.String(50))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+class RecipeStep(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    step = db.Column(db.Text, nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
