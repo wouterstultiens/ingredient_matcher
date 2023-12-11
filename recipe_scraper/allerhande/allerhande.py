8b34819b18d133eda3b1c4aa68e3fb6c441d39e6
Wouter Stultiens <wouterstultiens@gmail.com>
2023-12-11 19:52:22 +0100

    added multithreading and saving the results to csv

--- recipe_scraper/allerhande/allerhande.py
+++ recipe_scraper/allerhande/allerhande.py
@@ -4,131 +4,105 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+import csv
 
-def scrape_recipe_links(base_url, num_pages):
-    all_recipe_links = []
+def init_driver():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36')
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+def scrape_recipe_links(base_url, num_pages):
+    driver = init_driver()
+    all_recipe_links = []
 
-    for i in range(0, num_pages + 1):
-        url = f"{base_url}&page={i}" if i > 0 else base_url
-        print(f"Accessing URL: {url}")
-
-        driver.get(url)
+    for i in range(num_pages + 1):
+        driver.get(f"{base_url}&page={i}" if i > 0 else base_url)
         time.sleep(5)
 
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         for div in soup.find_all('div', class_='column xxlarge-4 large-6 small-12'):
-            try:
-                link_tag = div.find('a', href=True)
-                if link_tag:
-                    name = link_tag.get('aria-label', '').replace('Recept: ', '')
-                    link = "https://www.ah.nl" + link_tag.get('href', '')
-                    img_tag = div.find('img', class_='card-image-set_imageSet__Su7xI')
-                    image_src = img_tag.get('data-srcset', '').split(', ')[-1].split(' ')[0] if img_tag else ''
-
-                    if "recept/R-" in link:
-                        all_recipe_links.append({'name': name, 'link': link, 'image': image_src})
-                        print(f"Recipe: {name} retrieved")
-                    else:
-                        print(f"LINK NOT VALID: Recipe: {name}, URL: {url}")
-
-            except Exception as e:
-                print(f"Error processing a recipe link: {e}")
+            link_tag = div.find('a', href=True)
+            if link_tag and "recept/R-" in link_tag.get('href', ''):
+                name = link_tag.get('aria-label', '').replace('Recept: ', '')
+                link = "https://www.ah.nl" + link_tag['href']
+                img_tag = div.find('img', class_='card-image-set_imageSet__Su7xI')
+                image_src = img_tag['data-srcset'].split(', ')[-1].split(' ')[0] if img_tag else ''
+                print(f"Recipe: {name} - retrieved")
+                all_recipe_links.append({'name': name, 'link': link, 'image': image_src})
 
     driver.quit()
     return all_recipe_links
 
-def scrape_recipe_details(recipe_links):
-    all_recipes = []
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36')
+def scrape_recipe_details(driver, recipe_link):
+    driver.get(recipe_link['link'])
+    time.sleep(1)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # Simplified scraping logic
+    recipe_info = {
+        'name': recipe_link['name'],
+        'link': recipe_link['link'],
+        'time_prepare': get_time_element(soup, 0),
+        'time_wait': get_time_element(soup, 1),
+        'rating': get_rating(soup),
+        'tags': [tag.text for tag in soup.select('.recipe-header-tags_tags__DFsas li .recipe-tag_text__aKcWG')],
+        'servings': get_text(soup, '.recipe-ingredients_count__zS2P-'),
+        'ingredients': [(amount.text.strip(), name.text.strip()) for amount, name in zip(soup.select('.recipe-ingredients_ingredientsList__thXVo .ingredient_unit__-ptEq'), soup.select('.recipe-ingredients_ingredientsList__thXVo .ingredient_name__WXu5R'))],
+        'equipment': [item.text.strip() for item in soup.select('.recipe-ingredients_kitchen__Ag6XI p.typography_root__Om3Wh')],
+        'steps': [step.text.strip() for step in soup.select('.recipe-steps_step__FYhB8 p.typography_root__Om3Wh')],
+        'image': recipe_link['image'],
+    }
 
-    for recipe in recipe_links:
+    print(recipe_info)
+    return recipe_info
+
+def get_time_element(soup, index):
+    elements = soup.select('.recipe-header-time_timeLine__nn84w')
+    return elements[index].text if len(elements) > index else None
+
+def get_rating(soup):
+    star_full_count = len(soup.select('.allerhande-icon.svg.svg--svg_star'))
+    star_half_count = len(soup.select('.allerhande-icon.svg.svg--svg_star-half'))
+    return star_full_count + star_half_count / 2
+
+def get_text(soup, selector):
+    element = soup.select_one(selector)
+    return element.text if element else None
+
+def save_to_csv(detailed_recipes, filename='recipes.csv'):
+    keys = detailed_recipes[0].keys()
+    with open(filename, 'w', newline='', encoding='utf-8') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(detailed_recipes)
+
+def main():
+    base_url = "https://www.ah.nl/allerhande/recepten-zoeken?menugang=hoofdgerecht"
+    num_pages = 0
+
+    recipe_links = scrape_recipe_links(base_url, num_pages)
+
+    # Create a pool of drivers for concurrent scraping
+    drivers = [init_driver() for _ in range(len(recipe_links[:10]))]
+
+    # Use ThreadPoolExecutor to scrape recipe details concurrently
+    with ThreadPoolExecutor(max_workers=len(drivers)) as executor:
+        results = executor.map(scrape_recipe_details, drivers, recipe_links[:10])
+
+    # Close all drivers
+    for driver in drivers:
+        driver.quit()
+
+    detailed_recipes = list(results)
+    save_to_csv(detailed_recipes)
+
+    for recipe in detailed_recipes:
         print(recipe)
-        driver.get(recipe['link'])
-        time.sleep(1)
 
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
+if __name__ == "__main__":
+    main()
 
-        # Scraping time elements
-        time_elements = soup.select('.recipe-header-time_timeLine__nn84w')
-        time_prepare = time_elements[0].text if len(time_elements) > 0 else None
-        time_wait = time_elements[1].text if len(time_elements) > 1 else None
-
-        # Scraping star ratings
-        star_full_count = len(soup.select('.allerhande-icon.svg.svg--svg_star'))
-        star_half_count = len(soup.select('.allerhande-icon.svg.svg--svg_star-half'))
-        stars = star_full_count + star_half_count / 2
-
-        # Scraping the tags
-        tags_list = soup.select('.recipe-header-tags_tags__DFsas li')
-        tags = [tag.select_one('.recipe-tag_text__aKcWG').text for tag in tags_list if tag.select_one('.recipe-tag_text__aKcWG')]
-
-        # Scraping servings
-        servings = soup.select_one('.recipe-ingredients_count__zS2P-').text if soup.select_one('.recipe-ingredients_count__zS2P-') else None
-
-        # Scraping ingredients
-        ingredients = []
-        ingredients_list = soup.select('.recipe-ingredients_ingredientsList__thXVo .ingredient_name__WXu5R')
-        ingredients_amount = soup.select('.recipe-ingredients_ingredientsList__thXVo .ingredient_unit__-ptEq')
-        for amount, name in zip(ingredients_amount, ingredients_list):
-            ingredients.append((amount.text.strip(), name.text.strip()))
-
-        # Scraping kitchen equipment
-        kitchen_equipment = []
-        kitchen_div = soup.find('div', class_='recipe-ingredients_kitchen__Ag6XI')
-        if kitchen_div:
-            kitchen_items = kitchen_div.find_all('p', class_='typography_root__Om3Wh')
-            for item in kitchen_items:
-                kitchen_equipment.append(item.text.strip())
-
-        # Scraping the steps
-        steps = []
-        steps_div = soup.find_all('div', class_='recipe-steps_step__FYhB8')
-        for step_div in steps_div:
-            step_text = step_div.find('p', class_='typography_root__Om3Wh').text.strip()
-            steps.append(step_text)
-
-        recipe_info = {
-            'name': recipe['name'],
-            'link': recipe['link'],
-            'time_prepare': time_prepare,
-            'time_wait': time_wait,
-            'rating': stars,
-            'tags': tags,
-            'servings': servings,
-            'ingredients': ingredients,
-            'equipment': kitchen_equipment,
-            'steps': steps,
-            'image': recipe['image'],
-        }
-
-        all_recipes.append(recipe_info)
-
-    driver.quit()
-    return all_recipes
-
-# Base URL and number of pages
-base_url = "https://www.ah.nl/allerhande/recepten-zoeken?menugang=hoofdgerecht"
-num_pages = 0
-
-# Scrape recipe links
-recipe_links = scrape_recipe_links(base_url, num_pages)
-
-# Scrape details from each recipe link
-detailed_recipes = scrape_recipe_details(recipe_links[:10])
-
-# Print or process the scraped data
-for recipe in detailed_recipes:
-    print(recipe)
