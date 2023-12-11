d28413fb3594d4a4e2ce7475abcd2d98f43baa57
Wouter Stultiens <wouterstultiens@gmail.com>
2023-12-11 21:46:25 +0100

    fixed search bug with ingredients and recipes

--- app.py
+++ app.py
@@ -2,23 +2,11 @@ from flask import render_template, request, jsonify
 from config import app, db
 from models import Recipe, Ingredient
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
     query = request.args.get('q', '')
-    if query:
-        ingredient_list = Ingredient.query.with_entities(Ingredient.name).filter(Ingredient.name.contains(query)).distinct().all()
-        recipe_list = Recipe.query.with_entities(Recipe.name).filter(Recipe.name.contains(query)).distinct().all()
-        suggestions = [ingredient.name for ingredient in ingredient_list] + [recipe.name for recipe in recipe_list]
-        return jsonify(suggestions)
-    return render_template('index.html')
-
-@app.route('/search-results')
-def search_results():
-    query = request.args.get('q', '')
+    recipes = []
     if query:
         # Check if the query matches an ingredient
         matching_ingredient = Ingredient.query.filter(Ingredient.name.contains(query)).first()
@@ -29,8 +17,20 @@ def search_results():
             # Otherwise, search for recipes by name
             recipes = Recipe.query.filter(Recipe.name.contains(query)).all()
 
-        return render_template('search_results.html', recipes=recipes, query=query)
-    return render_template('index.html')
+    return render_template('index.html', recipes=recipes, query=query)
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q', '')
+    if query:
+        ingredient_list = Ingredient.query.with_entities(Ingredient.name).filter(
+            Ingredient.name.contains(query)).distinct().all()
+        recipe_list = Recipe.query.with_entities(Recipe.name).filter(Recipe.name.contains(query)).distinct().all()
+        suggestions = [ingredient.name for ingredient in ingredient_list] + [recipe.name for recipe in recipe_list]
+        return jsonify(suggestions)
+    return jsonify([])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/dynamic_fields.js
+++ static/dynamic_fields.js
@@ -1,32 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var searchInput = document.getElementById('quickSearchInput');
-    var searchButton = document.getElementById('searchButton');
+    let searchInput = document.getElementById('quickSearchInput');
+    let searchButton = document.getElementById('searchButton');
 
-    searchInput.addEventListener('input', function(event) {
-        fetchSuggestions(event.target);
-    });
-
-    searchButton.addEventListener('click', function() {
-        performSearch(searchInput.value);
-    });
-
-    document.addEventListener("click", function (e) {
-        closeAllLists(e.target);
-    });
+    searchInput.addEventListener('input', event => fetchSuggestions(event.target));
+    searchButton.addEventListener('click', () => performSearch(searchInput.value));
+    document.addEventListener("click", e => closeAllLists(e.target));
 });
 
 function fetchSuggestions(inputElement) {
-    var searchTerm = inputElement.value;
-    if (searchTerm.length < 1) {
-        closeAllLists();
-        return;
-    }
+    let searchTerm = inputElement.value;
+    if (searchTerm.length < 1) return closeAllLists();
 
     fetch('/search?q=' + encodeURIComponent(searchTerm))
         .then(response => response.json())
-        .then(suggestions => {
-            showSuggestions(inputElement, suggestions);
-        })
+        .then(suggestions => showSuggestions(inputElement, suggestions))
         .catch(error => console.error('Error fetching suggestions:', error));
 }
 
@@ -34,33 +21,39 @@ function showSuggestions(inputElement, suggestions) {
     closeAllLists();
     if (!suggestions.length) return;
 
-    var list = document.createElement("DIV");
+    let list = createList(inputElement);
+    suggestions.forEach(suggestion => createListItem(inputElement, list, suggestion));
+}
+
+function createList(inputElement) {
+    let list = document.createElement("DIV");
     list.setAttribute("id", inputElement.id + "autocomplete-list");
     list.setAttribute("class", "autocomplete-items");
     list.style.position = 'absolute';
     list.style.left = inputElement.getBoundingClientRect().left + 'px';
     list.style.top = (inputElement.getBoundingClientRect().top + inputElement.offsetHeight) + 'px';
     document.body.appendChild(list);
+    return list;
+}
 
-    var searchTerm = inputElement.value;
-    suggestions.forEach(function(suggestion) {
-        var item = document.createElement("DIV");
-        var regex = new RegExp("(" + searchTerm + ")", "gi");
-        item.innerHTML = suggestion.replace(regex, "<strong>$1</strong>");
-        item.addEventListener("click", function() {
-            window.location.href = '/search-results?q=' + encodeURIComponent(suggestion);
-            closeAllLists();
-        });
-        list.appendChild(item);
+function createListItem(inputElement, list, suggestion) {
+    let item = document.createElement("DIV");
+    item.innerHTML = highlightTerm(inputElement.value, suggestion);
+    item.addEventListener("click", () => {
+        performSearch(suggestion);
+        closeAllLists();
     });
+    list.appendChild(item);
+}
+
+function highlightTerm(term, text) {
+    return text.replace(new RegExp("(" + term + ")", "gi"), "<strong>$1</strong>");
 }
 
 function closeAllLists(elmnt) {
-    var items = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < items.length; i++) {
-        if (elmnt != items[i]) {
-            items[i].parentNode.removeChild(items[i]);
-        }
+    let items = document.getElementsByClassName("autocomplete-items");
+    for (let i = 0; i < items.length; i++) {
+        if (elmnt != items[i]) items[i].parentNode.removeChild(items[i]);
     }
 }
 
